3e271a806dd8f1bf05d0fd67b61b9e90760da3f7
AIDEN <76594690+aiden3493@users.noreply.github.com>
2022-04-03 12:44:35 +0900

    events patch

--- components/eventD-day.tsx
+++ components/eventD-day.tsx
@@ -1,26 +1,35 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const DDay = (props: any) => {
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [month, setMonth] = useState(
+    parseInt(new Date().getMonth().toString()) + 1
+  );
+  const [date, setDay] = useState(parseInt(new Date().getDate().toString()));
 
-    const [year, setYear] = useState(new Date().getFullYear())
-    const [month, setMonth] = useState(parseInt(new Date().getMonth().toString()) + 1)
-    const [date, setDay] = useState(parseInt(new Date().getDate().toString()))
+  const startDate = new Date(year, month, date);
+  const endDate = new Date(props.year, props.month, props.date);
 
-    const startDate = new Date(year, month, date)
-    const endDate = new Date(props.year, props.month, props.date)
+  const btMs = endDate.getTime() - startDate.getTime();
+  const btDay =
+    btMs / (1000 * 60 * 60 * 24) === 0 ? "Day" : btMs / (1000 * 60 * 60 * 24);
 
-    const btMs = endDate.getTime() - startDate.getTime()
-    const btDay = btMs / (1000 * 60 * 60 * 24) === 0 ? "Day" : btMs / (1000 * 60 * 60 * 24)
+  setInterval(() => setDay(parseInt(new Date().getDate().toString())), 1000);
 
-    setInterval(() => setDay(parseInt(new Date().getDate().toString())), 1000)
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h1 className={`${props.main ? "text-3xl" : "text-xl"}`}>
+        {props.event}
+      </h1>
+      <h1
+        className={
+          "text-ms"
+        }>{`${props.year}.${props.month}.${props.date}`}</h1>
+      <h1 className={`${props.main ? "text-4xl" : "text-2xl"} text-green-300`}>
+        {`D - ${btDay}`}
+      </h1>
+    </div>
+  );
+};
 
-    return (
-        <div className="flex flex-col items-center justify-center">
-            <h1 className={`${props.main ? "text-3xl" : "text-xl"}`}>{props.event}</h1>
-            <h1 className={"text-ms"}>{`${props.year}.${props.month}.${props.date}`}</h1>
-            <h1 className={`${props.main ? "text-4xl" : "text-2xl"} text-green-300`}>{`D - ${btDay}`}</h1>
-        </div>
-    )
-}
-
-export default DDay
+export default DDay;
--- components/events.tsx
+++ components/events.tsx
@@ -1,27 +1,37 @@
 import DDay from "./eventD-day";
 
 const Event = () => {
-    const mainEvent = {event: "만우절 / 팝스", year: 2022, month: 4, date: 1}
-    const nextEvents = [{event: "진로 체험의 날", year: 2022, month: 5, date: 3}]
+  const mainEvent = { event: "진로 체험의 날", year: 2022, month: 5, date: 3 };
+  const nextEvents = [{ event: "스포츠한마당", year: 2022, month: 5, date: 4 }];
 
-    return (
-        <div className=" w-[22.5rem] h-[22.5rem] rounded-[35px] border-2 border-solid p-[37px] shadow-xl">
-            <div className="text-center text-[40px] font-bold font-sans">
-                <span className="text-2xl text-center font-medium">
-                    school {" "}
-                </span>
-                Events
-            </div>
+  return (
+    <div className=' w-[22.5rem] h-[22.5rem] rounded-[35px] border-2 border-solid p-[37px] shadow-xl'>
+      <div className='text-center text-[40px] font-bold font-sans'>
+        <span className='text-2xl font-medium text-center'>school </span>
+        Events
+      </div>
 
-            <div
-                className="mt-3 w-full h-[220px] border-solid border-green-300 border-2 rounded-3xl select-text p-5 flex flex-col items-center font-bold flex flex-col justify-center items-center space-y-2">
-                <DDay event={mainEvent.event} year={mainEvent.year} month={mainEvent.month} date={mainEvent.date} main={true}/>
-                {nextEvents.map((element, index) =>
-                    <DDay key={index} event={element.event} year={element.year} month={element.month} date={element.date} main={false}/>
-                )}
-            </div>
-        </div>
-    )
-}
+      <div className='mt-3 w-full h-[220px] border-solid border-green-300 border-2 rounded-3xl select-text p-5 flex flex-col items-center font-bold justify-center space-y-2'>
+        <DDay
+          event={mainEvent.event}
+          year={mainEvent.year}
+          month={mainEvent.month}
+          date={mainEvent.date}
+          main={true}
+        />
+        {nextEvents.map((element, index) => (
+          <DDay
+            key={index}
+            event={element.event}
+            year={element.year}
+            month={element.month}
+            date={element.date}
+            main={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Event
+export default Event;
